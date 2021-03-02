b7d67617f42b0392d8a95ac6a2184ec45dbe1d9b
Stakhiev-Alexander <sashastakhiev@gmail.com>
2021-03-03 01:32:27 +0300

    Made video assembling a stage

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from stages.artefacts_stage import ArtefactsStage
+from stages.assemble_stage import AssembleStage
 from stages.process_sequence import ProcessSequence
 from stages.nlm_stage import NLMStage
 from stages.sr_stage import SRStage
 
 
 if __name__ == '__main__':
-    ps = ProcessSequence(input_path='/home/quadro/videoproc/datasets/umatic_frames/')
+    input_path = '~/videoproc/datasets/umatic_frames/'
+    ps = ProcessSequence(input_path=input_path)
     ps.add(NLMStage(grayscale=False))
     ps.add(SRStage())
+    ps.add(AssembleStage(framerate=25, filename='umatic_out'))
     ps.execute()
-
-    # assemble_video_lossless(imgs_path='/home/quadro/videoproc/video-processing/sr_stage_output/', framerate=25,filename='hockey_nlm_cb_sr')
---
+++ stages/assemble_stage.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+import utils.logger as logger
+from stages.sequence_stage_base import SequenceStage
+from utils.video_tools import assemble_video
+
+logger = logger.get_logger(__name__)
+
+
+class AssembleStage(SequenceStage):
+    def __init__(self, framerate, filename, output_path='./output/'):
+        self._framerate = framerate
+        self._filename = filename
+        self._output_path = str(Path(output_path).absolute())
+
+    def execute(self, input_path):
+        self._input_path = str(Path(input_path).absolute())
+
+        try:
+            assemble_video(imgs_path=self._input_path, framerate=self._framerate, filename=self._filename)
+        except Exception as err:
+            logger.error(err)
+            raise err
+
+    @property
+    def output_path(self):
+        return self._output_path
